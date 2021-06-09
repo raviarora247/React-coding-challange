4663eac79c933f25aea1a96a46147b23343fe5ef
raviarora247 <ravi.arora@247labs.com>
2021-06-09 13:24:13 +0530

    config added

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    publicRuntimeConfig: {
+      UNSPLASH_ACCESS_KEY: process.env.UNSPLASH_ACCESS_KEY,
+      UNSPLASH_SECRET_KEY: process.env.UNSPLASH_SECRET_KEY,
+    },
+  };
+  
