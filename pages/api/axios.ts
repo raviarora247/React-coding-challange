eea3d48efa319c40fe6be0dcf981f1435e34a46e
raviarora247 <ravi.arora@247labs.com>
2021-06-10 09:49:10 +0530

    cleanup

--- pages/api/axios.ts
+++ pages/api/axios.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import {  ImageProps } from "../../types";
 import getConfig from "next/config";
-const { publicRuntimeConfig } = getConfig();
+export const { publicRuntimeConfig } = getConfig();
 
 
-
-export const fetchData = async (link: string) => {
+export const fetchData = async (link : any) => {
     const key = publicRuntimeConfig.UNSPLASH_ACCESS_KEY;
-    const {data} = await axios.get(`https://api.unsplash.com/${link}&client_id=${key}`)
-    const typedDate: ImageProps[] = data
+    const { data } = await axios.get(`https://api.unsplash.com/${link}&client_id=${key}`);
+    console.log("data", data);
+    const typedDate: ImageProps[] = data;
     return typedDate
 }
